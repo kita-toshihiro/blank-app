c3f9157db5b58c9f7b2e1326d7a498e30433558d
Kita <ghub@tkita.net>
2026-05-30 22:01:33 +0900

    Fix typo in info message for file submission

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,7 +90,8 @@ def get_check_results(file_bytes, file_name):
 st.set_page_config(page_title="ODS Checker", layout="wide")
 
 st.title("📊 課題ファイル提出前の最低限のチェック")
-st.info("このサイトでは、３ブロック課題の提出ファイルが最低限の体裁を持っているかチェックします。３ブロック課題の**提出場所ではない**です。")
+st.info("このサイトでは、３ブロック課題の提出ファイルが最低限の体裁を持っているかチェックします。３ブロック課題の**提出場所ではありません.
+**")
 
 uploaded_file = st.file_uploader("チェックする３ブロック課題のODSファイルをアップロードしてください", type=["ods"])
 
