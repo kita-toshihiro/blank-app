9fbb67cb387fdfde745a43683a38e749b00b3903
Kita <ghub@tkita.net>
2026-05-30 22:21:48 +0900

    Update info message for file submission check

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,8 +90,7 @@ def get_check_results(file_bytes, file_name):
 st.set_page_config(page_title="ODS Checker", layout="wide")
 
 st.title("📊 課題ファイル提出前の最低限のチェック")
-st.info("このサイトでは、３ブロック課題の提出ファイルが最低限の体裁を持っているかチェックします。３ブロック課題の**提出場所ではありません.
-**")
+st.info("ここで、３ブロック課題の提出ファイルの最低限の体裁のチェックができます。課題の**提出はできません。**")
 
 uploaded_file = st.file_uploader("チェックする３ブロック課題のODSファイルをアップロードしてください", type=["ods"])
 
